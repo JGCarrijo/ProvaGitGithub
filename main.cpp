e6ec3972fc7971278e81bfd32f6ad4ec72ac7557
Joao Guilherme <carrijojg@gmail.com>
2020-05-19 08:01:32 -0300

    Cabecalho

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
+    cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
+    cout << "--------------------------------" << endl;
+    cout << "INFORME OS DADOS DO ULTIMO" << endl << endl;
 
     return 0;
 }
