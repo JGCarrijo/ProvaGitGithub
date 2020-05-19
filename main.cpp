8f5b832ef8a7a9cce26fcc0bdfa4f23288e23dff
Joao Guilherme <carrijojg@gmail.com>
2020-05-19 08:19:19 -0300

    Leitura do volume

--- main.cpp
+++ main.cpp
@@ -4,9 +4,17 @@ using namespace std;
 
 int main()
 {
+    int compra;
+    double TicketMedio;
+
     cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
     cout << "--------------------------------" << endl;
     cout << "INFORME OS DADOS DO ULTIMO" << endl << endl;
+
+    cout << "Quantas compras o cliente fez no ultimo ano? ";
+    cin >> compra;
+    cout << "Qual o ticket médio? ";
+    cin >> TicketMedio;
 
     return 0;
 }
